f0cc08e248b6d3538bdc6d5b8aed5efacce2f90b
panstone <panaskin.dim@yandex.ru>
2019-01-10 01:26:12 +0300

    second day

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
+	#главная страница
 	def index
-		
+		@post = Post.all
+	end
+	#добавление нового поста
+	def new
+
+	end
+	def show
+		@post = Post.find(params[:id])
+	end
+ 	#создание нового поста
+	def create
+	#	render plain: params[:post].inspect
+	@post = Post.new(post_params)
+
+	@post.save
+	redirect_to @post
+	end
+	def about
+
+	end
+	private def post_params
+		params.require(:post).permit(:title,:body)
 	end
 end
