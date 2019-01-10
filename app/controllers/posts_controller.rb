712bd61d9ee3463f7b53ad27f4edc89c9e964d8d
panstone <panaskin.dim@yandex.ru>
2019-01-11 00:34:02 +0300

    Edit and Delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,12 @@ class PostsController < ApplicationController
 	end
 	#добавление нового поста
 	def new
-
+		@post = Post.new
 	end
 	def show
+		@post = Post.find(params[:id])
+	end
+	def edit
 		@post = Post.find(params[:id])
 	end
  	#создание нового поста
@@ -15,8 +18,24 @@ class PostsController < ApplicationController
 	#	render plain: params[:post].inspect
 	@post = Post.new(post_params)
 
-	@post.save
+	if(@post.save)
 	redirect_to @post
+	else
+	render 'new'
+	end
+end
+def update
+	@post = Post.find(params[:id])
+	if(@post.update(post_params))
+	redirect_to @post
+	else
+	render 'edit'
+	end
+	end
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
 	end
 	def about
 
